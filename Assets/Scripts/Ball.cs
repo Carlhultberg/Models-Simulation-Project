5b3cf49ccdff909241ab40d361e27cd199942eaf
Fredrik Bixo <fredrikbixo@n151-p101.eduroam.kth.se>
2018-03-05 17:08:45 +0100

    Implemented collision detection

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public Vector3 initialVelocity;
+	public Vector3 gravityForce;
+	float mass = 23323;
+	// Use this for initialization
+	void Start () {
+		// Rigidbody body = new Rigidbody ();
+		gameObject.AddComponent<Rigidbody>();
+		gameObject.GetComponent<Rigidbody> ().useGravity = false;
+
+		gravityForce = new Vector3 (0f, 1f, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	//	transform.position = 
+		transform.position += initialVelocity * Time.deltaTime * 5;
+		transform.position -= gravityForce * Time.deltaTime * 1;
+
+	}
+}
---
+++ Assets/Scripts/Canon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Debug.Log ("Pressed left click.");
+
+			GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+			// Add euler angles
+			sphere.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z+3);
+
+			sphere.AddComponent<Ball>(); 
+			sphere.GetComponent<Ball>().initialVelocity = transform.up;
+			//Instantiate(sphere);
+
+		}
+			
+	}
+}
